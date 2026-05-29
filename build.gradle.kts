008d871821321ccdc32d8623eabc64729198420d
cold <mtabolucas@gmail.com>
2026-05-29 16:48:09 +0300

    Initial setup 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
-    id("maven-publish")
 }
 
 group = "com.mtabo.necta"
@@ -14,13 +13,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-    // Kotlin Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
-
-    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-
-    // Jsoup
     implementation("org.jsoup:jsoup:1.18.1")
 }
 
@@ -30,19 +24,4 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-/**
- * Publishing configuration (required for JitPack/Maven usage)
- */
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            from(components["kotlin"])
-
-            groupId = project.group.toString()
-            artifactId = "mtabo-necta"
-            version = project.version.toString()
-        }
-    }
 }
